a807c3f4b6c0d007519911dd95c7088a563869d2
onizuka <dark_sao@mail.ru>
2022-06-10 11:17:25 +0300

    initial commit

---
+++ remember.rb
@@ -0,0 +1,23 @@
+require 'set'
+# Я злой
+
+# def remember(str)
+#   arr = []
+#   str.chars.each_with_object(Hash.new(0)) { |x, h| h[x] += 1; arr << x if h[x] == 2 }
+#   return arr
+# end
+
+def remember(str)
+  str.chars.reduce([[], Set.new]) do |(arr, set), x|
+    arr << x if set.include?(x) && !arr.include?(x)
+    set << x
+    [arr, set]
+  end.first
+end
+
+p remember('apple') #, ["p"])
+p remember("apPle") #, [])
+p remember("pippi") #, ["p", "i"])
+p remember('Pippi') #, ["p", "i"])                                            {"i"=>2, "p"=>2}
+p remember("limbojackassin the garden") #, ["a", "s", "i", " ", "e", "n"])
+p remember("11pinguin") #, ["1", "i", "n"])
