780375d2b6a06021c023cccd0c45b53606ec48f1
onizuka <dark_sao@mail.ru>
2022-07-30 17:01:26 +0300

    add playing_with_passphrases

---
+++ playing_with_passphrases.rb
@@ -0,0 +1,35 @@
+def play_pass(str, n)
+  str.chars.map do |l|
+    if l =~ /[A-Za-z]/
+      ar = [l]
+      n.to_i.times { |x| ar << ar.last.next}
+      ar.last[-1]
+    elsif l =~ /\d/
+      9 - l.to_i
+    else
+      l
+    end
+  end.map.with_index { |x, i| i.odd? ? x.to_s.downcase : x }.reverse.join
+end
+  
+
+# Everyone knows passphrases. One can choose passphrases from poems, songs, movies names and so on but frequently 
+# they can be guessed due to common cultural references. You can get your passphrases stronger by different means. One is the following:
+
+# choose a text in capital letters including or not digits and non alphabetic characters,
+
+# shift each letter by a given number but the transformed letter must be a letter (circular shift),
+# replace each digit by its complement to 9,
+# keep such as non alphabetic and non digit characters,
+# downcase each letter in odd position, upcase each letter in even position (the first character is in position 0),
+# reverse the whole result.
+# Example:
+# your text: "BORN IN 2015!", shift 1
+
+# 1 + 2 + 3 -> "CPSO JO 7984!"
+
+# 4 "CpSo jO 7984!"
+
+# 5 "!4897 Oj oSpC"
+
+# With longer passphrases it's better to have a small and easy program. Would you write it?
